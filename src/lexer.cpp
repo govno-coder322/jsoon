e1575557ebb9f0870dcefbdaee539c4a28d259e3
govno-coder322 <govno-coder322@mail.com>
2025-06-29 21:26:43 +0500

    checkpoint

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -10,9 +10,17 @@ std::vector<Token> tokenize(const std::string& input) {
     while (i < input.size()) {
         switch (input[i]) {
             case '{':
-                tokens.push_back({ TokenType::LeftBrace, ""});
-                ++i;
-                break;
+                tokens.push_back({ TokenType::LeftBrace, ""}); ++i; break;
+            case '}':
+                tokens.push_back({ TokenType::RightBrace, ""}); ++i; break;
+            case ':':
+                tokens.push_back({ TokenType::Colon, ""}); ++i; break;
+            case ',':
+                tokens.push_back({ TokenType::Comma, ""}); ++i; break;
+            case '[':
+                tokens.push_back({ TokenType::RightBracket, ""}); ++i; break;
+            case ']':
+                tokens.push_back({ TokenType::LeftBracket, ""}); ++i; break;
             default:
                 ++i;
         }
